5a24c9c335fd32f8d036c9f301748c01c5e30d9d
Dima Lapchak <dimalapchak@MacBook-Pro-Dima.local>
2020-08-22 12:26:54 +0300

    Parsing comment

--- src/extension/script.js
+++ src/extension/script.js
@@ -5,6 +5,12 @@ $(document).ready(function (){
         let user = {};
         user = {
             "name": $(".comment > .b-post-author > .avatar ").eq(i).text().trim(),
+            "text": $(".comment > .comment_text ").eq(i).text().trim(),
+        }
+        if(user["name"] === $(".comment > .b-post-author > .avatar ").eq(i+1).text().trim()){
+            user["text_2"] = $(".comment > .comment_text").eq(i+1).text().trim();
+            i+=1;
+            console.log(i);
         }
         users.push(user);
     }
