f9489af412392ca7d24e59d2dc13e2b040f96058
Dima Lapchak <dimalapchak@MacBook-Pro-Dima.local>
2020-08-22 13:30:38 +0300

    delete spacing

--- extension/src/script.js
+++ extension/src/script.js
@@ -5,7 +5,6 @@ $(document).ready(function (){
         let user = {};
         user = {
             "name": $(".comment > .b-post-author > .avatar ").eq(i).text().trim(),
-
             "text": $(".comment > .comment_text ").eq(i).text().trim(),
         }
         if(user["name"] === $(".comment > .b-post-author > .avatar").eq(i+1).text().trim()){
