79b018d860a1e68c1f4ab7a4f5f2d41565eb0907
Dima Lapchak <dimalapchak@Dimas-MacBook-Pro.local>
2020-09-06 18:03:33 +0300

    change variable name

--- extension/src/script.js
+++ extension/src/script.js
@@ -2,6 +2,8 @@ $(document).ready(function (){
     $("head").append("<link rel='stylesheet' href='/css/jquery-ui.min.css'>");
     let users = [];
     let comments = $(".comment");
+    $(".bottom-line").append("<div class='show_comment'>Вернути</div>");
+    $(".show_comment").fadeOut().button();
     $('.bottom-line').append("<div class='slider-rating-max'>" +
         "<div class='slider-handle ui-slider-handle'></div>" +
         "</div>");
@@ -23,35 +25,31 @@ $(document).ready(function (){
                 handle.text(ui.value);
             },
             change: function( event, ui ) {
-                let value =  ui.value;
-                let handle = $(ui.handle);
+                let slider_value =  ui.value;
+                let slider_handle = $(ui.handle);
                 let comment = handle.parent().parent().parent();
-                let background = "";
-                if (value < 0) {
+                let background = null;
+                if (slider_value < 0) {
                     background = "red";
-                    if(value < -2){
-                        comment.hide(1000);
+                    if(slider_value < -2){
+                        comment.fadeOut(1000);
                     }
-                } else if (value > 0) {
-                    if (value > 0 && value < 3) {
+                } else if (slider_value > 0) {
+                    if (slider_value > 0 && slider_value < 3) {
                         background = "yellow";
-                    } else if (value >= 3 && value <= 4) {
+                    } else if (slider_value >= 3 && slider_value <= 4) {
                         background = "orange";
-                    } else if (value > 4) {
+                    } else if (slider_value > 4) {
                         background = "green";
                     }
                 }else {
                     background = "white";
                 }
-                if(background){
-                    handle.css("background", background);
-                    handle.parent().css("background", background);
-                    let string_border = value + " solid " + background;
-                    comment.css({
-                        border: string_border,
-                    });
-                }
-
+                slider_handle.css("background", background);
+                slider_handle.parent().css("background", background);
+                comment.css({
+                    border: slider_value +"px solid " + background,
+                });
             }
         });
         $(".b-post-author").eq(i).append("<span><b>Рейтинг:"+ rating +"</b></span>");
@@ -67,13 +65,27 @@ $(document).ready(function (){
         }
         users.push(user);
         if (slider.slider("value") < -2) {
-            $(".comment").eq(i).hide(1000);
+            $(".comment").eq(i).fadeOut(1000);
         }
     }
     $(".bottom-line").append("<div class='send-rating-button'>Оцінити</div>");
     $(".send-rating-button").click(function (e, obj) {
-        $(this).hide(1000);
-        $(this).parent().find($(".slider-rating-max")).hide(1000);
+        $(this).fadeOut(1000);
+        $(this).parent()
+            .find($(".slider-rating-max"))
+            .fadeOut(1000);
+        $(this).parent()
+            .find($(".show_comment"))
+            .fadeIn(2000);
     }).button();
+    $(".show_comment").click(function (){
+        $(this).fadeOut(1000);
+        $(this).parent()
+            .find($(".show_comment"))
+            .fadeOut(1000);
+        $(this).parent()
+            .find($(".slider-rating-max , .send-rating-button"))
+            .fadeIn(2000);
+    });
     console.log(users);
 });
